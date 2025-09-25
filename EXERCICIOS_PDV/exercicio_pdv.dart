a55aad916391991a986653189d87bc0727f75437
Madu0809 <116366672+Maria200608@users.noreply.github.com>
2025-09-25 00:50:54 +0000

    feat:continuação

--- EXERCICIOS_PDV/exercicio_pdv.dart
+++ EXERCICIOS_PDV/exercicio_pdv.dart
@@ -1,3 +1,4 @@
+import 'pdv'
 void main(){
 
 }
--- EXERCICIOS_PDV/pdv_app.dart
+++ EXERCICIOS_PDV/pdv_app.dart
@@ -1,4 +1,4 @@
-import 'dart.io';
+import 'dart:io';
 import 'produto.dart';
 import 'item_compra.dart';
 import 'carrinho_compra.dart';
@@ -22,9 +22,73 @@ class PDVApp{
            }else {
              continuar = false;
            }
+ }
+        if(carrinho.estaVazio()){
+            print('\nNenhum produto foi adicionado ao carrinho.');
+            print('\nObrigado por usar o sistema PDV. ');
+            }else{
+                exibirResultado();
+            }
 
+            ItemCompra lerDadosCompra(){
+                print('\n-- Adicionar Produto --');
+                print('\nNome do produto:');
 
+                String nome = stdin.readLineSync()? .toUpperCase() ?? ' ';
 
+                double  valor = 0;
+                bool valorValido - false;
+
+                while (!valorValido){
+                    print('\nValor unitário: ');
+                    String nome = stdin.readLineSync()? .toUpperCase() ?? ' ';
+                    valorUnitario = double.parse (imput.replaceALL(',','.'));
+                    if (valorUnitario <= 0){
+                        print('\nValor deve ser maior que zero.');
+                    }else{
+                        valorValido = true;
+
+                    } 
+                }
+
+                int quantidade = 0;
+                bool quantidadeValida = true;
+
+                while (!quantidadeValida){
+                    print('\nQuantidade:');
+                String input = stdin.readLineSync()? .toUpperCase() ?? '0';
+                quantidade = int.parse(input);
+                if (quantidade <= 0){
+                    print('\nErro: A quantidade deve ser maior que zero.');
+                }else{
+                    quantidadeValida = true;
+                }
+            }
+             
+             Produto produto = Produto(nome,valorUnitario);
+             return ItemCompra(produto, quantidade);
+            }
+            void exibirResultado(){
+                double valorFinal - carrinho.CalcularValorFinal();
+
+                print('\n-- RESUMO DA COMPRA==');
+                print('\nItens do carrinho');
+
+                for(int i = 0; i < carrinho.itens.length;i++){
+                    print('${i + 1}, $carrinho.itens[i]}');
+                }
+
+                print('\nTotal de itens: ${carrinho.quantidadeItens()}');
+                print('\nValor total: R\$ ${total.toStringAsFixed(2)}');
+
+                if(desconto > 0){
+                    print('\nDesconto (10%): R $ ${desconto.toStringAsFixed(2)}');
+                }else{
+                    print('\nDesconto: R\$ 0.00');
+                }
+
+                print('\nValor final: R\ $ ${valorFinal.toStringAsFixed(2)}');
+                print()
+            }
         }
-
     }
